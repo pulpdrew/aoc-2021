1ab9fb4d6a4f40347af9b6c1e0eab0392f3df34b
Andrew Davis <pulpdrew@gmail.com>
2021-12-25 13:23:36 -0500

    run all days in main()

--- aoc-2021.go
+++ aoc-2021.go
@@ -1,7 +1,57 @@
 package main
 
-import "pulpdrew/aoc-2021/day25"
+import (
+	"fmt"
+	"pulpdrew/aoc-2021/day01"
+	"pulpdrew/aoc-2021/day02"
+	"pulpdrew/aoc-2021/day03"
+	"pulpdrew/aoc-2021/day04"
+	"pulpdrew/aoc-2021/day05"
+	"pulpdrew/aoc-2021/day06"
+	"pulpdrew/aoc-2021/day07"
+	"pulpdrew/aoc-2021/day08"
+	"pulpdrew/aoc-2021/day09"
+	"pulpdrew/aoc-2021/day10"
+	"pulpdrew/aoc-2021/day11"
+	"pulpdrew/aoc-2021/day12"
+	"pulpdrew/aoc-2021/day13"
+	"pulpdrew/aoc-2021/day14"
+	"pulpdrew/aoc-2021/day15"
+	"pulpdrew/aoc-2021/day16"
+	"pulpdrew/aoc-2021/day17"
+	"pulpdrew/aoc-2021/day18"
+	"pulpdrew/aoc-2021/day19"
+	"pulpdrew/aoc-2021/day20"
+	"pulpdrew/aoc-2021/day21"
+	"pulpdrew/aoc-2021/day22"
+	"pulpdrew/aoc-2021/day23"
+	"pulpdrew/aoc-2021/day25"
+)
 
 func main() {
+	day01.Run()
+	day02.Run()
+	day03.Run()
+	day04.Run()
+	day05.Run()
+	day06.Run()
+	day07.Run()
+	day08.Run()
+	day09.Run()
+	day10.Run()
+	day11.Run()
+	day12.Run()
+	day13.Run()
+	day14.Run()
+	day15.Run()
+	day16.Run()
+	day17.Run()
+	day18.Run()
+	day19.Run()
+	day20.Run()
+	day21.Run()
+	day22.Run()
+	day23.Run()
+	fmt.Println("Day 24 - Use Python and Z3 to run the python script in the Day24/ directory")
 	day25.Run()
 }
